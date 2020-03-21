29e5b34accb495e5c997eabf63fbab9d40415116
moh <mo.souley@gmail.com>
2020-03-21 11:05:42 +0100

    first commit

---
+++ src/main/java/com/moh/springbootheroku/SpringbootherokuApplication.java
@@ -0,0 +1,13 @@
+package com.moh.springbootheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootherokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootherokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moh/springbootheroku/SpringbootherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moh.springbootheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootherokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
